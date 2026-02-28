86d703291777a8ae426a76f9645284285745e6cd
Quinn Weber <quinn@quinnweber.com>
2026-02-27 22:43:37 -0800

    fix api

--- internal/server/foo.go
+++ internal/server/foo.go
@@ -5,9 +5,9 @@ type FooInput struct {
 }
 
 type FooOutput struct {
-	Baz string
+	Baz string `json:"baz"`
 }
 
 func (s *ServerImpl) Foo(input FooInput) (FooOutput, error) {
-	return FooOutput{Baz: input.Bar + " world"}, nil
+	return FooOutput{Baz: input.Bar}, nil
 }
